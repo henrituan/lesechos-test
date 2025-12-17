9b696a15e5d8049c2ffd977d8883d294ed98872a
Henri Tuan <henri.tuan77@gmail.com>
2025-12-17 11:12:39 +0100

    deps: add panda css

---
+++ panda.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "@pandacss/dev";
+
+export default defineConfig({
+  preflight: true,
+  include: ['./src/components/**/*.{ts,tsx,js,jsx}', './src/app/**/*.{ts,tsx,js,jsx}'],
+  exclude: [],
+  theme: {
+    extend: {},
+  },
+  outdir: "styled-system",
+});
