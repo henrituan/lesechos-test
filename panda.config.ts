2d8e5bd0aef740c63f6c68ee62b753647aaba103
Henri Tuan <henri.tuan77@gmail.com>
2025-12-17 13:11:41 +0100

    feat: extends theme

--- panda.config.ts
+++ panda.config.ts
@@ -2,10 +2,36 @@ import { defineConfig } from "@pandacss/dev";
 
 export default defineConfig({
   preflight: true,
-  include: ['./src/components/**/*.{ts,tsx,js,jsx}', './src/app/**/*.{ts,tsx,js,jsx}'],
+  include: [
+    "./src/components/**/*.{ts,tsx,js,jsx}",
+    "./src/app/**/*.{ts,tsx,js,jsx}",
+  ],
   exclude: [],
+  conditions: {
+    extend: {
+      dark: "@media (prefers-color-scheme: dark)",
+    },
+  },
   theme: {
-    extend: {},
+    extend: {
+      semanticTokens: {
+        colors: {
+          bg: {
+            DEFAULT: {
+              value: { base: "white", _dark: "#191919" },
+            },
+            neutral: {
+              value: { base: "#f4f4f4", _dark: "#212121" },
+            },
+          },
+          text: {
+            DEFAULT: {
+              value: { base: "#212121", _dark: "white" },
+            },
+          },
+        },
+      },
+    },
   },
   outdir: "styled-system",
 });
