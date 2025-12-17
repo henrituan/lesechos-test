b5a4ae40b7b055e49d4ee2c373188bc5ceee1e18
Henri Tuan <henri.tuan77@gmail.com>
2025-12-17 16:40:00 +0100

    fix: ArticleCard title position

--- src/app/(newsletter)/_components/ArticleCard/index.tsx
+++ src/app/(newsletter)/_components/ArticleCard/index.tsx
@@ -11,42 +11,55 @@ type ArticleCardProps = {
 export const ArticleCard = ({ title, description, image, ctaType }: ArticleCardProps) => {
   return (
     <div className={css({
-      position: 'relative',
       display: 'flex',
       flexDirection: 'column',
       gap: '2',
     })}>
-      <Link
-        href="/"
-        style={{ backgroundImage: `url(${image}?random=${Math.random()})` }}
-        className={
-          css({
-            display: "block",
-            width: "100%",
-            height: "200px",
-            bgSize: "cover",
-            bgPosition: "center",
-            opacity: "1",
-            transition: "opacity 0.3s ease-in-out",
-            _hover: {
-              opacity: "0.5",
-            }
-          })
-        }
-      />
-
-      <h3 className={css({
-        position: 'absolute',
-        top: '30%',
-        left: '0',
-        right: '0',
-        color: 'white',
-        textShadow: '1px 1px 2px rgba(0, 0, 0, 0.5)',
-        textAlign: 'center',
-        fontSize: 'title.lg',
-        fontWeight: 'bold',
-        pointerEvents: "none",
-      })}>{title}</h3>
+      <div className={css({
+        position: 'relative',
+        overflow: 'hidden',
+      })}>
+        <Link
+          href="/"
+          style={{ backgroundImage: `url(${image}?random=${Math.random()})` }}
+          className={
+            css({
+              display: "block",
+              width: "100%",
+              height: "200px",
+              bgSize: "cover",
+              bgPosition: "center",
+              opacity: "1",
+              transition: "opacity 0.3s ease-in-out",
+              _hover: {
+                opacity: "0.5",
+              }
+            })
+          }
+        >
+        </Link>
+        <div className={css({
+          position: 'absolute',
+          top: '0',
+          left: '0',
+          right: '0',
+          height: '200px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          pointerEvents: "none",
+        })}>
+          <h3 className={css({
+            textShadow: '1px 1px 2px rgba(0, 0, 0, 0.5)',
+            textAlign: 'center',
+            fontSize: 'title.lg',
+            fontWeight: 'bold',
+            color: 'white',
+          })}>
+            {title}
+          </h3>
+        </div>
+      </div>
 
       <p className={css({
         color: "text",
